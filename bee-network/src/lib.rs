2816973e4dcc8e2298e3dbda71c6086ee8b87cc2
/alex/ <alexander.schmidt@iota.org>
2020-11-17 14:37:57 +0100

    * Add ReadableId trait; * impl ReadableId for PeerId; * Add readable PeerIds to error messages

--- bee-network/src/lib.rs
+++ bee-network/src/lib.rs
@@ -106,3 +106,28 @@ pub async fn init(config: NetworkConfig, local_keys: Keypair, shutdown: &mut Shu
         event_receiver,
     )
 }
+
+pub trait ReadableId {
+    const OriginalLength: usize;
+    const ReadableLength: usize;
+
+    type Identifier: ToString;
+
+    fn readable(&self) -> String;
+}
+
+impl ReadableId for PeerId {
+    const OriginalLength: usize = 52;
+    const ReadableLength: usize = 8;
+
+    type Identifier = PeerId;
+
+    fn readable(&self) -> String {
+        let s = self.to_base58();
+        format!(
+            "{}*{}",
+            &s[0..2],
+            &s[(Self::OriginalLength - Self::ReadableLength + 2)..]
+        )
+    }
+}
